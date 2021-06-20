56c267ed3472a5e4948ba82da8e2b82c0aef5230
YudhiPermanaPBO1 <yudhi.gotan.10@gmail.com>
2021-06-20 21:57:56 +0800

    Tampil Data (JFrame)

---
+++ src/id/ac/uniska/Main.java
@@ -0,0 +1,11 @@
+package id.ac.uniska;
+
+import id.ac.uniska.model.MataKuliah;
+import id.ac.uniska.ui.MataKuliahRead;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
